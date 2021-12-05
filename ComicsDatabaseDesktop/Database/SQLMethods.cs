a921ddf336ef2239de480c96197761229b87fc56
Richard Dubný <richarddubny18@gmail.com>
2021-12-05 20:29:22 +0100

    Revert "Revert "Add project files.""
    This reverts commit f78840bce87eccf2cfc0910f14639a6e3a3c3d37.

---
+++ ComicsDatabaseDesktop/Database/SQLMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ComicsDatabaseDesktop.Database
+{
+    class SQLMethods
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        public  SQLMethods()
+        {
+            Initilize();
+        }
+        public void Initilize()
+        {
+            server = "hopefully running";
+            database = "komiksy";
+            uid = "root";
+            password = "|pryxi!Am4yZxE0N}";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+        }
+    }
+        }
